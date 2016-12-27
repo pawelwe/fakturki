d449c40d3c58b792fe6c9eaf0172be744cd7c365
Pawel Wegrzyniak <pwegrzyniak@gmail.com>
2016-12-27 22:43:39 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <header class="container">
+      <img src="./assets/logo.png">
+    </header>
+    <main class="container">
+      <comp-1></comp-1>
+      <comp-1></comp-1>
+      <comp-1></comp-1>
+      <comp-1></comp-1>
+    </main>
+    <footer class="container">
+      Copyright
+    </footer>
+
+
+  </div>
+
+</template>
+
+<script>
+  import comp1 from './Components/Comp1.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      'comp-1': comp1
+    },
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import './scss/_custom.scss';
+</style>
---
+++ src/components/Comp1.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'Hello comp2'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import '../scss/_custom.scss';
+
+</style>
