3568de7a20b56364ba5148ddb151954ebbab8ce5
Pawel Wegrzyniak <pwegrzyniak@gmail.com>
2016-12-30 23:15:07 +0100

    Calculations fix

--- src/components/NewInvoice.vue
+++ src/components/NewInvoice.vue
@@ -1,24 +1,24 @@
 <template>
   <main class="invoice">
     <section>
-      <textarea class="invoice-from u-text-right " rows="1" :placeholder="invoiceTemplate.executive"></textarea>
-      <textarea class="invoice-to u-text-left" rows="7" :placeholder="invoiceTemplate.recipient"></textarea>
+      <textarea class="invoice-from u-text-right " rows="1" :placeholder="invoiceTemplate.executive" v-model="invoiceTemplate.executive"></textarea>
+      <textarea class="invoice-to u-text-left" rows="7" :placeholder="invoiceTemplate.recipient" v-model="invoiceTemplate.recipient"></textarea>
     </section>
     <section class="invoice-data">
-      <textarea class="u-text-center invoice-header" rows="1" :placeholder="invoiceTemplate.invoiceTitle"></textarea>
+      <textarea class="u-text-center invoice-header" rows="1" :placeholder="invoiceTemplate.invoiceTitle" v-model="invoiceTemplate.invoiceTitle"></textarea>
       <div class="u-text-left invoice-date">
-        <textarea class="u-text-left invoice-date-date" rows="1">{{ invoiceTemplate.invoiceDate }}</textarea>
+        <textarea class="u-text-left invoice-date-date" rows="1" :placeholder="invoiceTemplate.invoiceDate" v-model="invoiceTemplate.invoiceDate"></textarea>
       </div>
       <div class="invoice-calc">
         <div class="invoice-calc-row invoice-calc-row__header">
-          <textarea rows="1" class="invoice-calc-input invoice-calc-number">{{ invoiceTemplate.labels.nrLabel }}</textarea>
-          <textarea rows="1" class="invoice-calc-input invoice-calc-name">{{ invoiceTemplate.labels.serviceNameLabel }}</textarea>
-          <textarea rows="1" class="invoice-calc-input invoice-calc-count">{{ invoiceTemplate.labels.ammountLabel }}</textarea>
-          <textarea rows="1" class="invoice-calc-input invoice-calc-price-netto">{{ invoiceTemplate.labels.priceNettoLabel }}</textarea>
-          <textarea rows="1" class="invoice-calc-input invoice-calc-value-netto">{{ invoiceTemplate.labels.valueNettoLabel }}</textarea>
-          <textarea rows="1" class="invoice-calc-input invoice-calc-vat">{{ invoiceTemplate.labels.valRateLabel }}</textarea>
-          <textarea rows="1" class="invoice-calc-input invoice-calc-vat-value">{{ invoiceTemplate.labels.valValueLabel }}</textarea>
-          <textarea rows="1" class="invoice-calc-input invoice-calc-full-value">{{ invoiceTemplate.labels.fullValueLabel }}</textarea>
+          <textarea rows="1" class="invoice-calc-input invoice-calc-number" :placeholder="invoiceTemplate.labels.nrLabel"></textarea>
+          <textarea rows="1" class="invoice-calc-input invoice-calc-name" :placeholder="invoiceTemplate.labels.serviceNameLabel"></textarea>
+          <textarea rows="1" class="invoice-calc-input invoice-calc-count" :placeholder="invoiceTemplate.labels.ammountLabel"></textarea>
+          <textarea rows="1" class="invoice-calc-input invoice-calc-price-netto" :placeholder="invoiceTemplate.labels.priceNettoLabel"></textarea>
+          <textarea rows="1" class="invoice-calc-input invoice-calc-value-netto" :placeholder="invoiceTemplate.labels.valueNettoLabel"></textarea>
+          <textarea rows="1" class="invoice-calc-input invoice-calc-vat" :placeholder="invoiceTemplate.labels.valRateLabel"></textarea>
+          <textarea rows="1" class="invoice-calc-input invoice-calc-vat-value" :placeholder="invoiceTemplate.labels.valValueLabel"></textarea>
+          <textarea rows="1" class="invoice-calc-input invoice-calc-full-value" :placeholder="invoiceTemplate.labels.fullValueLabel"></textarea>
         </div>
         <div class="invoice-calc-row">
           <span class="invoice-calc-input invoice-calc-number">1</span>
@@ -43,14 +43,12 @@
           <span class="invoice-calc-output invoice-calc-full-value">{{ bruttoValue }}</span>
         </div>
       </div>
-
       <section class="invoice-summary">
-        <span rows="1" class="invoice-summary-row u-bold invoice-calc-label">Do zapłaty: {{ bruttoValue }}</span>
-        <textarea rows="1" class="invoice-summary-row u-text-xs" placeholder="Słownie: sto złoty osiem groszy"></textarea>
-        <textarea rows="1" class="invoice-summary-row u-bold u-mt-15" placeholder="Sposób płatności: przelew na konto"></textarea>
-        <textarea rows="1" class="invoice-summary-row u-text-xs" placeholder="Numer konta: (mbank) XX XXXX XXXX XXXX XXXX XXXX XXXX"></textarea>
+        <span rows="1" class="invoice-summary-row u-bold invoice-calc-label">Do zapłaty: {{ bruttoValue }} zł</span>
+        <textarea rows="1" class="invoice-summary-row u-text-xs" :placeholder="invoiceTemplate.valueInWords" v-model="invoiceTemplate.valueInWords"></textarea>
+        <textarea rows="1" class="invoice-summary-row u-bold u-mt-15" :placeholder="invoiceTemplate.paymentType" v-model="invoiceTemplate.paymentType"></textarea>
+        <textarea rows="1" class="invoice-summary-row u-text-xs" :placeholder="invoiceTemplate.accountNumber" v-model="invoiceTemplate.accountNumber"></textarea>
       </section>
-
     </section>
     <footer class="placeholder"></footer>
   </main>
@@ -63,13 +61,13 @@
         return this.$store.getters.template
       },
       nettoValue () {
-        return parseInt((this.invoiceTemplate.labels.ammountValue * this.invoiceTemplate.labels.priceNettoValue).toFixed(2))
+        return parseInt((this.invoiceTemplate.labels.ammountValue * this.invoiceTemplate.labels.priceNettoValue)).toFixed(2)
       },
       vatValue () {
-        return parseInt((this.nettoValue * (0 + '.' + parseInt(this.invoiceTemplate.labels.vatRateValue.replace(/%/g, '')))).toFixed(2))
+        return parseInt((this.nettoValue * (0 + '.' + parseInt(this.invoiceTemplate.labels.vatRateValue.replace(/%/g, ''))))).toFixed(2)
       },
       bruttoValue () {
-        return this.nettoValue + this.vatValue
+        return (parseInt(this.nettoValue) + parseInt(this.vatValue)).toFixed(2)
       }
     }
   }
