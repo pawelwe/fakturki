a5e4565fcbfcbb373f7160aaaba5e768cc70d153
Pawel Wegrzyniak <pwegrzyniak@gmail.com>
2017-01-07 00:59:56 +0100

    Start page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="invoice">
+    <main class="start-page">
       <header></header>
       <h1 class="u-text-center">Welcome to <span class="u-violet u-bold">FAKTURKI!</span><span class="u-text-xs u-top"> ©</span></h1>
       <!-- FOOTER PLACEHOLDER (FLEX) -->
