3a97300b3af4b4d8523cac5051c76f7ec770ee5d
Pawel Wegrzyniak <pwegrzyniak@gmail.com>
2017-01-06 21:46:42 +0100

    1 module out

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-// import invoicesList from './modules/invoicesList'
-import invoiceTemplate from './modules/invoiceTemplate'
+import invoice from './modules/invoice'
 import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    invoiceTemplate
+    invoice
   },
   plugins: [createPersistedState()]
 })
