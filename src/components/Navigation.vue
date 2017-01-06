bae2248ffcd602b2206d45a1184f14829f0e2eee
Pawel Wegrzyniak <pwegrzyniak@gmail.com>
2017-01-06 20:24:57 +0100

    Menu text

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -7,7 +7,7 @@
     <nav class="main-nav-menu">
       <ul class="main-nav-menu-list">
         <li class="main-nav-menu-list-item" v-if="$route.path === '/'">
-          <router-link class="main-nav-menu-list-item-link" to="/nowa-fakturka">Nowa fakturka start</router-link>
+          <router-link class="main-nav-menu-list-item-link" to="/nowa-fakturka">Nowa fakturka</router-link>
         </li>
         <li class="main-nav-menu-list-item" @click="loadTemplate" v-if="$route.path !== '/'">
           <router-link class="main-nav-menu-list-item-link" to="/nowa-fakturka">Nowa fakturka</router-link>
