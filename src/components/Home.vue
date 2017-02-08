c32adaec98350542f14b5248d56ebabb1780b45f
Pawel Wegrzyniak <pwegrzyniak@gmail.com>
2017-02-08 17:25:22 +0100

    Start page css flex

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,6 @@
 <template>
     <main class="start-page">
-      <header></header>
       <h1 class="u-text-center">Welcome to <span class="u-violet u-bold">FAKTURKI!</span><span class="u-text-xs u-top"> ©</span></h1>
-      <!-- FOOTER PLACEHOLDER (FLEX) -->
-      <footer class="placeholder"></footer>
     </main>
 </template>
 
